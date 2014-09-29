77db3faada2d69d174f29bd9a0e8b06bf8eb0d84
Pippin Williamson <pippin@pippinsplugins.com>
2014-09-29 14:33:03 -0500

    Version bump and changelog for v1.1.1

--- edd-discounts.php
+++ edd-discounts.php
@@ -6,7 +6,7 @@
  * Author:              Chris Christoff
  * Author URI:          http://www.chriscct7.com
  *
- * Version:             1.1
+ * Version:             1.1.1
  * Requires at least:   3.9
  * Tested up to:        4.0
  *
@@ -79,7 +79,7 @@ class edd_dp {
 		}
 		// Plugin Version
 		if ( ! defined( 'EDD_DP_PLUGIN_VERSION' ) ) {
-			define( 'EDD_DP_PLUGIN_VERSION', '1.1' );
+			define( 'EDD_DP_PLUGIN_VERSION', '1.1.1' );
 		}
 		// Plugin Root File
 		if ( ! defined( 'EDD_DP_PLUGIN_FILE' ) ) {
