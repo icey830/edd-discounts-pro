943bdad421d9f9c7f6f3f42a55e0976e8273ffb8
Chris Christoff <chriscct7@users.noreply.github.com>
2016-07-18 15:44:00 -0400

    Update class-discounts.php

--- classes/class-discounts.php
+++ classes/class-discounts.php
@@ -397,11 +397,11 @@ class EDD_Discounts {
 		}
 
 		// check start and end dates
-		if ( $discount['start'] !== '' && strtotime( $discount['start'] ) > current_time( "timestamp") ) {
+		if ( $discount['start'] !== '' && (int) strtotime( $discount['start'] ) > (int) current_time( "timestamp" ) ) {
 			return false;
 		}
 
-		if ( $discount['end'] !== '' && strtotime( $discount['end'] ) < current_time( "timestamp") ) {
+		if ( $discount['end'] !== '' && (int) strtotime( $discount['end'] ) < (int) current_time( "timestamp" ) ) {
 			return false;
 		}
 
