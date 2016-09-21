b62983e7640e1b035f6744bf4ffb67aa7b1506cb
Chris Christoff <hello@chriscct7.com>
2016-09-20 20:15:47 -0400

    Better date and time validation

--- classes/class-discounts.php
+++ classes/class-discounts.php
@@ -401,12 +401,27 @@ class EDD_Discounts {
 		}
 
 		// check start and end dates
-		if ( $discount['start'] !== '' && (int) strtotime( $discount['start'], current_time( "timestamp" ) ) > (int) current_time( "timestamp" ) ) {
-			return false;
-		}
-
-		if ( $discount['end'] !== '' && (int) strtotime( $discount['end'], current_time( "timestamp" ) ) < (int) current_time( "timestamp" ) ) {
-			return false;
+		$start 		  = $discount['start'];
+		$end 		  = $discount['end'];
+		$current_time = (int) current_time( "timestamp" );
+		if ( $start == '' && $end == '' ){ // Both not set
+			// fine
+		} else if ( $start !== '' && $end == '' ) { // Only start set
+			if ( (int) strtotime( $start, $current_time ) >= $current_time ) {
+				return false; // Waiting to begin
+			}
+		} else if ( $start == '' && $end !== '' ) { // Only end set
+			if ( (int) strtotime( $end, $current_time ) <= $current_time ) {
+				return false; // Finished
+			}
+		} else { // Both set
+			if ( (int) strtotime( $start, $current_time ) >= $current_time ) {
+				if ( (int) strtotime( $end, $current_time ) <= $current_time ) { // started and done
+					return false; // Finished
+				}
+			} else {
+				return false; // Invalid time settings or waiting to begin
+			}
 		}
 
 		// if discount is only for previous customers and customer does not have any previous purchases
